a1b3e913ea433516f7bb26db70c203bb603ba081
Jorge Sanchez <lilium277@gmail.com>
2015-12-02 18:09:04 -0600

    Se actualiza el archivo pruebas para incluir listar y eliminar

--- ropa/tests.py
+++ ropa/tests.py
@@ -1,10 +1,23 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
-from ropa.views import nuevo , listar
+from ropa.views import nuevo, listar, eliminar
 
 # Create your tests here.                                       
 class MainPageRopa(TestCase):
 
-	def test_root_url_resolves_to_nuevo_view(self):
+	def test_root_url_resolves_to_ropa_nuevo_view(self):
 		found = resolve('/ropa/nuevo/')
 		self.assertEqual(found.func, nuevo)
+
+	def test_root_url_resolves_to_ropa_listar_view(self):
+		found = resolve('/ropa/')
+		self.assertEqual(found.func, listar)
+
+	def test_root_url_resolves_to_ropa_eliminar_view(self):
+		found = resolve('^/ropa/eliminar/')
+		self.assertEqual(found.func, eliminar)
+
+	# def test_root_url_resolves_to_ropa_editar_view(self):
+	#	found = resolve('/ropa/editar/')
+	#	print found
+	#	self.assertEqual(found.func, editar)
